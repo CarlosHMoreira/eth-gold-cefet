79ebd42646e5fa53896574acdc6af16f14202c0f
Carlos Moreira <kmoreira620@gmail.com>
2018-11-02 18:55:08 -0300

    Creates layout component

---
+++ src/app/core/layout/layout.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cft-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements OnInit {
+
+  title = 'Eth-Cefet Gold Coin';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
