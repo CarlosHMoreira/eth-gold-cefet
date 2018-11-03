c012898cf0937457168b46a3fb0768fa48bd9d5e
Unknown <kmoreira620@gmail.com>
2018-11-02 21:18:19 -0300

    Import RouterModule to allow use of router directives

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './layout/layout.component';
+import { RouterModule } from '@angular/router';
+
 import { MaterialModule } from '../material/material.module';
+
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule
   ],
   declarations: [LayoutComponent],
   exports: [
