bfdaebeafebff96d142ecff0ec967ff63fd3fee7
Carlos Moreira <carlos.moreira@infobase.com.br>
2018-11-21 13:12:11 -0200

    Imports angular forms modules

--- src/app/transfer/transfer.module.ts
+++ src/app/transfer/transfer.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TransferComponent } from './transfer/transfer.component';
 import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ],
   declarations: [TransferComponent],
   exports: [TransferComponent]
