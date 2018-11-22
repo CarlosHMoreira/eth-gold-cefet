fb610264bdc02121d7823953a9ccf87ca2748c51
Carlos Moreira <carlos.moreira@infobase.com.br>
2018-11-22 13:13:19 -0200

    List accounts and adds a functions to handler the transaction.

--- src/app/transfer/transfer/transfer.component.ts
+++ src/app/transfer/transfer/transfer.component.ts
@@ -12,16 +12,28 @@ import { Web3Service } from '../../shared/services/web3.service';
 export class TransferComponent implements OnInit {
 
   transferForm: FormGroup;
+  accounts = this.web3Service.accountsObservable;
+
   constructor(
     private formBuilder: FormBuilder,
-    // private web3Service: Web3Service,
+    private web3Service: Web3Service,
     ) { }
 
   ngOnInit() {
+
+    this.buildForm();
+  }
+
+  private buildForm() {
     this.transferForm = this.formBuilder.group({
       from: [null, Validators.required],
       amount: [0, Validators.min(1)],
       to: ['', Validators.required]
     });
   }
+
+  transferGold() {
+
+  }
+
 }
