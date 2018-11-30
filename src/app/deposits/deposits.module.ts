951a84cd6bbe704744847d67a2bb67f075119c4e
Carlos Moreira <carlos.moreira@infobase.com.br>
2018-11-29 22:54:07 -0200

    Creates deposits module and component

---
+++ src/app/deposits/deposits.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DepositComponent } from './deposit/deposit.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DepositComponent]
+})
+export class DepositsModule { }
