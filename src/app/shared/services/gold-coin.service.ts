b6cbe1bc450358d907eead7171812d9affef6d8b
Carlos Moreira <carlos.moreira@infobase.com.br>
2018-11-30 00:58:13 -0200

    Creates getBalance method

--- src/app/shared/services/gold-coin.service.ts
+++ src/app/shared/services/gold-coin.service.ts
@@ -25,15 +25,22 @@ export class GoldCoinService {
   sendCoin(transfer: Transfer) {
     console.log(`Sending ${transfer.amount} coins to ${transfer.receiver}`);
     console.log('Initiating transaction... (please wait)');
-        return this.deployedContract.sendCoin
-          .sendTransaction(transfer.receiver, transfer.amount, {from: transfer.sender});
+    return this.deployedContract.sendCoin
+      .sendTransaction(transfer.receiver, transfer.amount, {from: transfer.sender});
   }
 
   depositCoin(deposit: Transfer) {
     console.log(`Depositing ${deposit.amount} coins to ${deposit.receiver}`);
     console.log('Initiating transaction... (please wait)');
-      return this.deployedContract.depositGold
-          .sendTransaction(deposit.receiver, deposit.amount, {from: this.web3Service.accounts[0]});
+    return this.deployedContract.depositGold
+      .sendTransaction(deposit.receiver, deposit.amount, {from: this.web3Service.accounts[0]});
+  }
+
+  getBalance(address: string) {
+    console.log(`Consulting balance of ${address}`);
+    console.log('Initiating transaction... (This is faster)');
+    return this.deployedContract.getBalance
+      .call(address);
   }
 
 }
