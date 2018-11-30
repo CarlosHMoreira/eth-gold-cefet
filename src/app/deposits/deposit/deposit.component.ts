bdf4b46b3e93900389618e6e6325d3a4b67897e7
Carlos Moreira <carlos.moreira@infobase.com.br>
2018-11-30 00:22:07 -0200

    Creates Deposit component logic and template

--- src/app/deposits/deposit/deposit.component.ts
+++ src/app/deposits/deposit/deposit.component.ts
@@ -1,4 +1,8 @@
+import { Transfer } from './../../shared/models/transfer';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { GoldCoinService } from '../../shared/services/gold-coin.service';
+import { SnackbarService } from '../../shared/services/snackbar.service';
 
 @Component({
   selector: 'cft-deposit',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepositComponent implements OnInit {
 
-  constructor() { }
+  depositForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackbarService: SnackbarService,
+    private goldCoinService: GoldCoinService
+    ) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm() {
+    this.depositForm = this.formBuilder.group({
+      amount: [0, [Validators.min(1), Validators.required]],
+      receiver: ['', Validators.required]
+    });
+  }
+
+  depositGold() {
+    this.depositForm.disable();
+
+    const deposit = this.depositForm.getRawValue() as Transfer;
+
+    this.goldCoinService
+      .depositCoin(deposit)
+      .catch(error => {
+        this.snackbarService.message('Transaction failed!');
+        this.depositForm.enable();
+        throw new Error(error);
+      })
+      .then(transaction => {
+        this.depositForm.reset();
+        this.depositForm.enable();
+        this.snackbarService.message(`Transaction successfull, ${transaction}`);
+      });
   }
 
 }
