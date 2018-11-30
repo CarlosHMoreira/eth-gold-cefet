dfc6bdecd4ed9c188dcb6b64fb39fa225f27bae5
Carlos Moreira <carlos.moreira@infobase.com.br>
2018-11-30 02:13:48 -0200

    Creates method to get Balance of  a address

--- src/app/shared/services/gold-coin.service.ts
+++ src/app/shared/services/gold-coin.service.ts
@@ -3,44 +3,46 @@ import { Injectable } from '@angular/core';
 import { Web3Service } from './web3.service';
 import { Transfer } from './../models/transfer';
 import GOLDCOIN_ARTIFACTS from '../../../../build/contracts/GoldCoin.json';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoldCoinService {
 
-  private deployedContract;
+  private goldCoinAbstraction;
 
   constructor(private web3Service: Web3Service) {
+    this.extractContractAbstraction();
+  }
+
+  private extractContractAbstraction() {
     this.web3Service.artifactsToContract(GOLDCOIN_ARTIFACTS)
-      .then(goldCoinAbstraction => goldCoinAbstraction.deployed())
-      .then(deployedContract => this.deployedContract = deployedContract)
-      .then( () => {
-        if (!this.deployedContract) {
-           throw new Error('GoldCoin is not loaded, unable to send transaction');
-        }
-      });
+      .then(goldCoinAbstraction => this.goldCoinAbstraction = goldCoinAbstraction);
   }
 
   sendCoin(transfer: Transfer) {
     console.log(`Sending ${transfer.amount} coins to ${transfer.receiver}`);
     console.log('Initiating transaction... (please wait)');
-    return this.deployedContract.sendCoin
-      .sendTransaction(transfer.receiver, transfer.amount, {from: transfer.sender});
+    return this.goldCoinAbstraction
+      .deployed()
+      .sendCoin
+        .sendTransaction(transfer.receiver, transfer.amount, {from: transfer.sender});
   }
 
   depositCoin(deposit: Transfer) {
+
     console.log(`Depositing ${deposit.amount} coins to ${deposit.receiver}`);
     console.log('Initiating transaction... (please wait)');
-    return this.deployedContract.depositGold
-      .sendTransaction(deposit.receiver, deposit.amount, {from: this.web3Service.accounts[0]});
+
+    return this.goldCoinAbstraction
+      .deployed()
+      .depositGold
+        .sendTransaction(deposit.receiver, deposit.amount, {from: this.web3Service.accounts[0]});
   }
 
   getBalance(address: string) {
-    console.log(`Consulting balance of ${address}`);
-    console.log('Initiating transaction... (This is faster)');
-    return this.deployedContract.getBalance
-      .call(address);
+    return this.goldCoinAbstraction.deployed()
+      .then(instance => instance.getBalance.call(address));
   }
-
 }
